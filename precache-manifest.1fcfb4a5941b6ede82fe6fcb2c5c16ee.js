eecb8308f1b54de915f992937729b8c1ad951516
quang.tran <quang.tran@aioz.io>
2020-12-16 18:03:22 +0700

    Init Willogy Scholarship 2020 page

---
+++ precache-manifest.1fcfb4a5941b6ede82fe6fcb2c5c16ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f21ca8ff36cd08e72de586bd1d73297",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51c619a3e12cba146b8a",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "46bf72ae9db766de6c18",
+    "url": "./static/css/main.11fa1427.chunk.css"
+  },
+  {
+    "revision": "51c619a3e12cba146b8a",
+    "url": "./static/js/2.987d7708.chunk.js"
+  },
+  {
+    "revision": "cbe01fbbe035bf90d35a835621f136ef",
+    "url": "./static/js/2.987d7708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bf72ae9db766de6c18",
+    "url": "./static/js/main.7aafa0e2.chunk.js"
+  },
+  {
+    "revision": "cac4e95983c728edc155",
+    "url": "./static/js/runtime-main.d7733466.js"
+  },
+  {
+    "revision": "772d8354308a781a18e406901c13fb09",
+    "url": "./static/media/logo.772d8354.svg"
+  },
+  {
+    "revision": "677c94f942bcdf43c01ebe98be0ed595",
+    "url": "./static/media/meadow.677c94f9.jpg"
+  },
+  {
+    "revision": "13360278cd457b49b84590e3051bc61f",
+    "url": "./static/media/tech-skills.13360278.png"
+  }
+]);
